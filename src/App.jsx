1d965af3fc77f4024a83275ea0ba0e8d8997eb2b
Bouchtarate <hamza.bouchtarate@gmail.com>
2023-06-13 15:22:09 +0100

    learn how use state hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Tweet from "./components/Tweet";
 import "./App.css";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const decrease = () => {
+    setCount(count - 1);
+  };
+  const increase = () => {
+    setCount(count + 1);
+  };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      name: "hamza",
+      message: "Hi im new here",
+      like: 4,
+    },
+    {
+      id: 2,
+      name: "aya",
+      message: "Hi hamza",
+      like: 1,
+    },
+    {
+      id: 3,
+      name: "ziad",
+      message: "welcome hamza",
+      like: 42,
+    },
+  ]);
   return (
-    <div className="tweet">
-      <Tweet name="Hamza" message="Hi im new here" like="4" />
-      <Tweet name="Aya" message="Welcome Hamza" like="1" />
-      <Tweet name="Ziad" message="Hi Hamza" like="70" />
-    </div>
+    <>
+      <div className="tweet">
+        {users.map((user) => (
+          <Tweet
+            key={user.id}
+            name={user.name}
+            message={user.message}
+            like={user.like}
+          />
+        ))}
+        <div className="count">
+          <button onClick={decrease}>-</button>
+          {count}
+          <button onClick={increase}>+</button>
+        </div>
+      </div>
+    </>
   );
 };
 
